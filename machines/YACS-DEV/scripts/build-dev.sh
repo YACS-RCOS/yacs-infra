745bfcfbd595d50b4564245b536cd3fc059ce9a4
Jacob Shomstein <jacob@shomstein.com>
2019-10-16 00:09:18 -0400

    removed other folder that are temp, bad execution on branching

--- machines/YACS-DEV/scripts/build-dev.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# BRANCH=$1
-# template later for multi branch dev env's
-
-docker build . -t "yacs-dev"
--- machines/YACS-DEV/scripts/start-dev.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# BRANCH=$1
-# PORT=$2
-# template later for multi branch dev env's
-
-docker run -d -p yacs-dev
