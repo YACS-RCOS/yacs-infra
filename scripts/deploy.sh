e9a72be1580d8fe111f79b63ea9523629ee5dede
Jacob Shomstein <jacob@shomstein.com>
2019-11-08 22:18:41 -0500

    updated deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# Example run: bash deploy.sh youngr7@yacs.cs.rpi.edu production
+# Example run: bash deploy.sh user@yacs.cs.rpi.edu "production" "master"
 
-SSHHOST=$1; ENV=$2
+SSHHOST=$1; ENV=$2; BRANCH=$3
 echo "SSHHOST: $SSHHOST"
 echo "ENV: $ENV"
 # ssh into server and run update-services script
-ssh $SSHHOST < update-services-$ENV.sh
+ssh $SSHHOST 'bash -s' -- < update-services-development.sh $ENV $BRANCH
