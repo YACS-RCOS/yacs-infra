24de4cdc99663090fd429bcacd9f3dbdaccce1de
Jacob Shomstein <jacob@shomstein.com>
2019-12-03 16:41:31 -0500

    added small docs

---
+++ scripts/example.update-services.sh
@@ -0,0 +1,4 @@
+# (from project yacs root)
+# NOTE: `develop` is the name of the main dev branch, this is auto deployed to kvm3.cs.rpi.edu
+# for prod: $ bash ./update-services.sh production master
+# for dev: $ bash ./update-services.sh development develop
